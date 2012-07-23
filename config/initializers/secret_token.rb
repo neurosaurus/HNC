687ec939d5b4f71ef8112e41c48079b24f4ba9af
Apprentice <apprentice@dbc13.devbootcamp.com>
2012-07-23 10:38:49 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HNC::Application.config.secret_token = '4f751d7818e25eae341573f1a51748028b60edc90bc5411a69b29faff0f6fce532dfe9fab441c150703e583087d3df950b5f337eeffd591dfb9ce7aa4b4192c8'
